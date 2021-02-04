1865593b3fb953c8018b6b49690edc931b2824e6
JKKiukas <jonne.kiukas@hotmail.com>
2021-02-04 13:47:03 +0200

    Menu list initial commit.

---
+++ MenuList/MenuList/MenuListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuListApp.swift
+//  MenuList
+//
+//  Created by Jonne Kiukas on 4.2.2021.
+//
+
+import SwiftUI
+
+@main
+struct MenuListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
