cadbca6058bdb7850944772bf41afcf4fbbca180
JKKiukas <jonne.kiukas@hotmail.com>
2021-02-04 15:15:50 +0200

    Added item row. Items which are listed now have a picture and a price.

---
+++ MenuList/MenuList/ItemRow.swift
@@ -0,0 +1,29 @@
+//
+//  ItemRow.swift
+//  MenuList
+//
+//  Created by Jonne Kiukas on 4.2.2021.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var item: MenuItem
+    
+    var body: some View {
+        HStack{
+            Image(item.thumbnailImage)
+            
+            VStack(alignment: .leading){
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
