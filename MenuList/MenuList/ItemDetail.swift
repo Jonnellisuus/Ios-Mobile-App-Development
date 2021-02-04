aacad2244102b1f9896d1ca87195471f1165a1a8
JKKiukas <jonne.kiukas@hotmail.com>
2021-02-04 17:04:02 +0200

    Added order view and initialized to work order.

--- MenuList/MenuList/ItemDetail.swift
+++ MenuList/MenuList/ItemDetail.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ItemDetail: View {
+    @EnvironmentObject var order: Order
     var item: MenuItem
     
     var body: some View {
@@ -24,6 +25,11 @@ struct ItemDetail: View {
             
             Text(item.description)
                 .padding()
+            
+            Button("Order this") {
+                self.order.add(item: self.item)
+            }.font(.headline)
+            
             Spacer()
             
         }.navigationBarTitle(Text(item.name), displayMode: .inline)
@@ -31,9 +37,11 @@ struct ItemDetail: View {
 }
 
 struct ItemDetail_Previews: PreviewProvider {
+    static let order = Order()
+    
     static var previews: some View {
         NavigationView{
-            ItemDetail(item: MenuItem.example)
+            ItemDetail(item: MenuItem.example).environmentObject(order)
         }
     }
 }
