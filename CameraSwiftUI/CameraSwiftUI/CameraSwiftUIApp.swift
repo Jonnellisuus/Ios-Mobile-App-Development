d2c771905f4a446556ee5d3ceaf291d8aa5ea014
JKKiukas <jonne.kiukas@hotmail.com>
2021-02-12 16:31:44 +0200

    Camera SwiftUI task initialized.

---
+++ CameraSwiftUI/CameraSwiftUI/CameraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSwiftUIApp.swift
+//  CameraSwiftUI
+//
+//  Created by Jonne Kiukas on 12.2.2021.
+//
+
+import SwiftUI
+
+@main
+struct CameraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraSwiftUI/CameraSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraSwiftUI
+//
+//  Created by Jonne Kiukas on 12.2.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
