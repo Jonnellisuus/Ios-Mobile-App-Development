d84aa7c9b018b0846bd6b243ed2fed6f4a643c7a
JKKiukas <jonne.kiukas@hotmail.com>
2021-03-15 15:10:57 +0200

    Added data structure.

---
+++ DigiTraffic/DigiTraffic/Data/DigiTrafficData.swift
@@ -0,0 +1,41 @@
+//
+//  DigiTrafficData.swift
+//  DigiTraffic
+//
+//  Created by Jonne Kiukas on 15.3.2021.
+//
+
+import Foundation
+
+struct DigiTrafficData: Decodable {
+    let features: [Feature]
+}
+
+struct Feature: Decodable, Identifiable {
+    let id: String
+    let properties: Property
+    
+    enum CodingKeys: String, CodingKey {
+        case id, properties
+    }
+}
+
+struct Property: Decodable, Identifiable {
+    let id, municipality, province: String
+    let names: Names
+    let presets: [Preset]
+
+    enum CodingKeys: String, CodingKey {
+        case id, municipality, province
+        case names, presets
+    }
+}
+
+struct Names: Decodable {
+    let fi: String?
+}
+
+struct Preset: Decodable, Identifiable {
+    let id = UUID()
+    let presentationName, imageUrl: String?
+}
